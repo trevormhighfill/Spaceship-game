1807f7d529f683ce32b1f3fc390f368a10776c74
trevormhighfill <129008272+trevormhighfill@users.noreply.github.com>
2024-05-19 09:00:42 -0500

    player bullets
    player can now shoot bullets

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
-
-var MAX_SPEED = 800
+var DAMAGE = 10
+var bullet = preload("res://bullet.tscn")
+var health = 100
+var max_health = 100
+var MAX_SPEED = 1000
 var ANGULAR_SPEED = .05
 var ACCELERATION = 1500
 var DRAG = 100
@@ -10,12 +13,20 @@ var current_speed = 0.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 #var aim = get_global_transform().basis
 
+func hit_with_bullet(damage,team,bullet):
+	if team == false:
+		bullet.queue_free()
+		health -= damage
+		if health <= 0:
+			print("GAMEOVER!!!!!!!!!!!")
+			queue_free()
+
 func _physics_process(delta):
-	
-	# Get the input direction and handle the movement/deceleration.
+	# Get thge input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var rdirection = Input.get_axis("rotate_right","rotate_left")
 	if rdirection:
+		
 		rotate(-rdirection * ANGULAR_SPEED)
 	else:
 		rotate(0)
@@ -29,5 +40,11 @@ func _physics_process(delta):
 		else:
 			velocity = Vector2.ZERO
 	
+	if Input.is_action_pressed("fire"):
+		var instance = bullet.instantiate()
+		instance.position = global_transform.basis_xform(Vector2.UP)*100 + position
+		instance.start_up(DAMAGE,true)
+		instance.rotation = rotation
+		get_parent().add_child(instance)
 	
 	move_and_slide()
---
+++ bullet.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+var SPEED = 30
+var DAMAGE : float = 10
+
+var player_team : bool
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func start_up(set_damage,team):
+	player_team = team
+	DAMAGE = set_damage
+	if !player_team:
+		$Sprite2D.modulate = Color(1,0,0,1)
+	else:
+		$Sprite2D.modulate = Color(0,.5,1,1)
+
+func _physics_process(delta):
+	position += global_transform.basis_xform(Vector2.UP)*SPEED
+
+
+func timer_timeout():
+	queue_free()
+
+
+func area_entered(area):
+	area.hit_with_bullet(DAMAGE,player_team,self)
+	print("banana ", DAMAGE)
+	
